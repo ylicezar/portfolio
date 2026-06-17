6bb9900018900e975d63a264e74f88a8adef2a32
yulia <yli-cezar@yandex.ru>
2026-06-17 23:21:17 +0300

    fix navigation menu import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router";
-import Nav from "../components/Nav";
+import NavigationMenu from "./components/navigation-menu";
 import Footer from "../components/Footer";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#F7F8FA]">
-      <Nav />
+      <NavigationMenu />
       <Outlet />
       <Footer />
     </div>
