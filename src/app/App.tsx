63ccfc46842aa277bdb4c6ba8c2e135fb40ff104
yulia <yli-cezar@yandex.ru>
2026-06-18 23:01:18 +0300

    fix app layout and remove broken navigation menu

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,27 @@
-import { Outlet } from "react-router";
-import NavigationMenu from "./components/ui/navigation-menu";
+import { Outlet, Link } from "react-router";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#F7F8FA]">
-      <NavigationMenu />
-      <Outlet />
+    <div className="min-h-screen bg-[#F7F8FA] flex flex-col">
+      
+      {/* Простое меню */}
+      <header className="w-full px-6 py-4 border-b bg-white flex gap-6">
+        <Link to="/" className="text-sm font-medium hover:opacity-70">
+          Главная
+        </Link>
+
+        <Link to="/cases" className="text-sm font-medium hover:opacity-70">
+          Кейсы
+        </Link>
+      </header>
+
+      {/* Контент страниц */}
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      {/* Футер */}
       <Footer />
     </div>
   );
