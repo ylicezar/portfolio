ab0a562dd24018416cbce0e84bcf79bddb925b10
yulia <yli-cezar@yandex.ru>
2026-06-26 22:44:16 +0300

    fix: remove Competencies section from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -890,7 +890,7 @@ function Home() {
       <Hero />
       <Stats />
       <About />
-      <Competencies />
+     /* <Competencies /> */
       <Projects />
       <Experience />
       <Tools />
