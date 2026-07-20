33e0d9c97f33919385d782a2bcd419e5ad5e2d6e
yulia <yli-cezar@yandex.ru>
2026-07-20 16:45:43 +0300

    Правки в роутере

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,7 +6,7 @@ import ProjectDetails from "../pages/ProjectDetails";
 import CaseAudiometr from "../pages/CaseAudiometr";
 import CaseDetiKlinika from "../pages/CaseDetiKlinika";
 import CaseInteracoustics from "../pages/CaseInteracoustics";
-import Cases from "./pages/Cases";
+import Cases from "../pages/Cases";
 
 export const router = createBrowserRouter([
   {
@@ -18,7 +18,7 @@ export const router = createBrowserRouter([
       { path: "/cases/audiometr", element: <CaseAudiometr /> },
       { path: "/cases/deti-klinika", element: <CaseDetiKlinika /> },
       { path: "/cases/interacoustics", element: <CaseInteracoustics /> },
-      { path: "cases", element: <Cases />,
+      { path: "/cases", element: <Cases />,
 },
     ],
   },
